ed3dfc178fecb8777ec0fd5d61b2ebbc97c53b8f
Lea-Mary <ld2871@srmist.edu.in>
2026-03-24 14:54:05 +0530

    lea Added feature/UC1-WelcomePage

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,11 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        // UC1 - Welcome Page
+        System.out.println("==================================");
+        System.out.println("   Welcome to Book My Stay App   ");
+        System.out.println("==================================");
+        System.out.println("Find your perfect stay anytime, anywhere!");
+    }
+}
